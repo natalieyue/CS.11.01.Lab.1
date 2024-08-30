45d3638968a98c9bb1f434939068bd6974b1383c
natalie <nataliey2027@student.cis.edu.hk>
2024-08-30 13:05:54 +0800

    yay

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
+
 public class Dateable {
 
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your age:\t");
+        int age = scanner.nextInt();
+
+        int minDateAge = (int) (age / 2.0) + 7;
+
+        System.out.printf("%d-year olds should date somebody who is at least %d years old.%n", age, minDateAge);
+
+
+
     }
 }
--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
+
 public class Pages {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your age:  ");
+        int age = scanner.nextInt();
+
+        int bookPages = (int) 100-age;
+
+        System.out.printf("%d-year olds should read %d pages before giving up on a book", age, bookPages);
 
     }
 }
